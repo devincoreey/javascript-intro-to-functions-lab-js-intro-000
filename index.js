e361ce936ef2294375ebbfbf3b373d2630fdcdf4
devin williams <williams.devin702@gmail.com>
2020-06-10 06:52:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(word){
   console.log(whisper(word))
 }
 function sayHiToGrandma(word){
-  if word.toLowerCase(){
+  if (word.toLowerCase()){
     return "I can't hear you"
   }
