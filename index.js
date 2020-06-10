2ee836a39f94ac7ab04fd4820975e6d855a6bf67
devin williams <williams.devin702@gmail.com>
2020-06-10 06:35:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(word){
+  return word.toUpperCase()
+}
+function whisper(word){
+  return word.toLowerCase()
+}
+function logShout(word){
+  console.log(shout(word))
+}
+function logWhisper(word){
+  console.log(whisper(word))
+}
+function sayHiToGrandma(word){
+  if {
+    return "I can't hear you!"
+  }
+}
